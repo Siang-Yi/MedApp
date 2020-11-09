60f53d618b6d1027ac6acfcba842924772adb6dc
Woon Siang Yi <swoo0012@student.monash.edu>
2020-11-09 17:10:04 +0800

    Update gitignore

---
+++ MedAppExe/dist/main/history.py
@@ -0,0 +1,7 @@
+class History:
+    def __init__(self, ic, date, symptom, medicine, note):
+        self.ic = ic
+        self.date = date
+        self.symptom = symptom
+        self.medicine = medicine
+        self.note = note
